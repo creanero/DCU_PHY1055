c3d0b0b64211f680df368e8781b5a8cd2025de63
Oisín Creaner <creanero@gmail.com>
2024-09-11 17:30:56 +0100

    Created function outlines in for exercise R2.

---
+++ R2_function_outlines.py
@@ -0,0 +1,36 @@
+# This file isn't a complete template, but rather gives an outline of what you need the function to do.
+# If you're feeling ambitious, you can keep these functions in a separate file and import them.
+# Look up how to do so for yourself, or experiment based on what you see in library imports
+
+
+def differential_rl(v, r, l, i):
+    """
+    Calculates the change in current over time from the formula
+    L(dI/dt) = V - RI
+    :param v: Float for voltage
+    :param r:
+    :param l:
+    :param i:
+    :return:
+    """
+    # complete the docstring
+    # do some maths to calculate the difference
+    # return the difference
+    pass # this is a placeholder. Replace it with a return value
+
+
+def exact_solution_rl(v, r, l, t):
+    """
+    Calculates the change in current over time from the formula
+    I = (V/R)(1-exp(-Rt/L))
+    :param v: Float for voltage
+    :param r:
+    :param l:
+    :param t:
+    :return:
+    """
+    # complete the docstring
+    # do some maths to calculate the exact solution at a given time
+    # return the difference
+    pass # this is a placeholder. Replace it with a return value
+
